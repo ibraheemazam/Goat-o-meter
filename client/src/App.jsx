59731624d17ad86cb19cc86a95605907dbb03bed
Ibraheem Azam <ibraheemazam@gmail.com>
2022-08-09 14:21:59 -0700

    Added basic strucuture of boxes to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { ChakraProvider, Flex, Heading } from '@chakra-ui/react';
+import { ChakraProvider, Box, Flex, Heading } from '@chakra-ui/react';
 
 
 const App = function App() {
   return (
     <ChakraProvider>
-      <Heading display="flex" justifyContent="center" margin="3vw">
-        Goat-o-meter
-      </Heading>
-      <div>Ibby was here and hes the goatttt</div>
+      <Box m="3vw" mb="6vw">
+        <Heading display="flex" justifyContent="center">
+          Goat-o-meter
+        </Heading>
+      </Box>
+      <Box m="3vw">
+        <div>Ibby was here and hes the goatttt</div>
+      </Box>
+      <Box m="3vw">
+        <div>add player cards here</div>
+      </Box>
     </ChakraProvider>
   );
 };
